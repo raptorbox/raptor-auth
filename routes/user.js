47b54bc8035db93072e7d608354a8e347f409587
lcapra <lcapra@fbk.eu>
2017-12-19 16:05:35 +0100

    fix roles parse

--- routes/user.js
+++ routes/user.js
@@ -3,42 +3,6 @@ module.exports.router = (router) => {
     const logger = require('../logger')
     const api = require('../api')
 
-    /**
-     * @swagger
-     * definitions:
-     *   LoginRequest:
-     *     type: object
-     *     required:
-     *       - username
-     *       - password
-     *     properties:
-     *       username:
-     *         type: string
-     *       password:
-     *         type: string
-     *         format: password
-     *   User:
-     *     allOf:
-     *       - $ref: '#/definitions/LoginRequest'
-     *       - required:
-     *         - id
-     *         - email
-     *         - enabled
-     *         - roles
-     *       - properties:
-     *         id:
-     *           type: string
-     *         email:
-     *           type: string
-     *           format: email
-     *         enabled:
-     *           type: boolean
-     *         roles:
-     *           type: array
-     *           items:
-     *             type: string
-     */
-
     router.get('/', function(req, res) {
         const q = {}
         if (req.query.username) {
@@ -52,28 +16,6 @@ module.exports.router = (router) => {
             })
     })
 
-    /**
-     * @swagger
-     * /user:
-     *   post:
-     *     tags:
-     *       - User
-     *     description: Creates a new user
-     *     produces:
-     *       - application/json
-     *     parameters:
-     *       - name: user
-     *         description: User object
-     *         in: body
-     *         required: true
-     *         schema:
-     *           $ref: '#/definitions/User'
-     *     responses:
-     *       200:
-     *         description: Created
-     *         schema:
-     *           $ref: '#/definitions/User'
-     */
     router.post('/', function(req, res) {
 
         const user = Object.assign({}, req.body)
